0825fedcb6ca02a6a9d41be775c8dde74720dba3
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-01-18 17:22:00 -0300

    Update webpack.config.js
    Update to use Sass

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,18 @@ module.exports = {
             {
                 use: 'babel-loader',
                 test: /\.js$/,
-                exclude: /node_modules/
-            }
+                exclude: /node_modules/,
+            }, {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+              },
         ]
     }
 };
